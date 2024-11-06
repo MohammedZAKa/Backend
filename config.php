d1c87d9b07ecc0763f3eb1d799677a5c9626c5a6
MohammedZAKa <mz8099413@gmail.com>
2024-11-06 21:20:04 +0200

    ok

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_NAME', 'mulitusermusic');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('HOST_NAME', 'localhost');
+
+define('APP_NAME','MUSIC WEBSITE');
+define('APP_DESC','this is website for musics');
---
+++ footer.php
@@ -0,0 +1,45 @@
+<div class="class_51" >
+							<div class="class_52" >
+								
+								<?php if(!is_logged_in()):?>
+								<a href="login.php" class="class_53"  >
+									Login
+								</a>
+							
+								<a href="signup.php" class="class_53"  >
+									Signup
+								</a>
+							<?php else:?>
+								<a href="profile.php" class="class_53"  >
+									profile
+								</a>
+								<a href="index.php" class="class_53"  >
+									Home
+								</a>
+							<?php endif;?>
+							</div>
+							<div class="class_52" >
+								<a href="#" class="class_53"  >
+									Top 20
+								</a>
+								<a href="#" class="class_53"  >
+									Popular
+								</a>
+								<a href="#" class="class_53"  >
+									Contact us
+								</a>
+							</div>
+							<div class="class_52" >
+								<a href="#" class="class_53"  >
+									Terms &amp; Conditions
+								</a>
+								<a href="#" class="class_53"  >
+									About us
+								</a>
+								<a href="#" class="class_53"  >
+									Latest
+								</a>
+							</div>
+						</div>
+	</body>
+</html>
